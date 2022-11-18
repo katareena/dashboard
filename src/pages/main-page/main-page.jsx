c60dabbba05c5966a6dc56aced7e3bfaa77af861
katareena <w10160177@gmail.com>
2022-11-18 18:49:01 +0300

    add react-div-100vh, testing 100vh on mobile

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import './main-page.scss'
+import './main-page.scss';
+import Div100vh from 'react-div-100vh';
 import Header from '../../components/header/header';
 import Sidebar from '../../components/sidebar/sidebar';
 import { Outlet } from 'react-router-dom';
 
 function MainPage() {
   return (
-    <div className='main-page'>
-      <Sidebar></Sidebar>
-      
-      <div className='main-page__inner'>
-        <Header />
+    <Div100vh>
+      <div className='main-page'>
+        <Sidebar></Sidebar>
+        
+        <div className='main-page__inner'>
+          <Header />
 
-        <main className='main-page__main'>
-          <Outlet />
-        </main>       
+          <main className='main-page__main'>
+            <Outlet />
+          </main>       
+        </div>
       </div>
-    </div>
-
+    </Div100vh>
   )
 }
 
