281ba1e719199dec66412c4b2bb55e93087fd29a
katareena <w10160177@gmail.com>
2022-11-18 19:21:42 +0300

    progress

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './main-page.scss';
-import { use100vh } from 'react-div-100vh'
+// import { use100vh } from 'react-div-100vh';
+import { useWindowSize } from '../../hooks/use-window-size';
 import Header from '../../components/header/header';
 import Sidebar from '../../components/sidebar/sidebar';
 import { Outlet } from 'react-router-dom';
 
 function MainPage() {
-  const height = use100vh();
-  const contetntHeight = height - '60px';
+  const [width, height] = useWindowSize();
+  // const height = use100vh();
+  const contetntHeight = width < 576 ? height - '60' : height;
 
   return (
     <div className='main-page' style={{height: contetntHeight}}>
