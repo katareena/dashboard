e8146d9ec150c7762fc9007f7deeb73db996f282
katareena <w10160177@gmail.com>
2022-11-18 19:06:56 +0300

    testing

--- src/pages/main-page/main-page.jsx
+++ src/pages/main-page/main-page.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
 import './main-page.scss';
-import Div100vh from 'react-div-100vh';
+import { use100vh } from 'react-div-100vh'
 import Header from '../../components/header/header';
 import Sidebar from '../../components/sidebar/sidebar';
 import { Outlet } from 'react-router-dom';
 
 function MainPage() {
-  return (
-    <Div100vh>
-      <div className='main-page'>
-        <Sidebar></Sidebar>
-        
-        <div className='main-page__inner'>
-          <Header />
+  const height = use100vh();
+  const contetntHeight = height - '60px';
 
-          <main className='main-page__main'>
-            <Outlet />
-          </main>       
-        </div>
+  return (
+    <div className='main-page' style={{contetntHeight}}>
+      <Sidebar></Sidebar>
+      
+      <div className='main-page__inner'>
+        <Header />
+
+        <main className='main-page__main'>
+          <Outlet />
+        </main>       
       </div>
-    </Div100vh>
+    </div>
   )
 }
 
