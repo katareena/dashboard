3a4987ef568d7459e7449a51fdc256fc72911923
katareena <w10160177@gmail.com>
2022-11-19 10:00:04 +0300

    redesign of mobile header

--- src/components/user-nav/user-nav.jsx
+++ src/components/user-nav/user-nav.jsx
@@ -1,27 +1,39 @@
 import React from 'react';
+import { useWindowSize } from '../../hooks/use-window-size';
 import { Link } from 'react-router-dom';
 import './user-nav.scss';
 import { ReactComponent as NotificationsIcon } from '../../assets/icons/notifications-icon.svg';
 import { ReactComponent as EditIcon } from '../../assets/icons/edit-icon.svg';
+import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow-icon.svg';
 import cn from 'classnames';
+import { breakpointWidth } from '../../constants/constants';
 
 function UserNav({user, notifications}) {
   const isNotifications = notifications.length !== 0;
+  const [width, ] = useWindowSize();
 
   return (
     <ul className='user-nav'>
-      <li className={cn('user-nav__item', {'user-nav__item--notifications': isNotifications})}>
-        <Link className='user-nav__link' to='/profile/notification' aria-label='view notifications'>
-          <NotificationsIcon />
-        </Link>
-      </li>
-      <li className='user-nav__item'>
-        <Link className='user-nav__link' to='/profile/edit' aria-label='edit your personal profile'>
-          <EditIcon />
-        </Link>
-      </li>
+
+      { width >= breakpointWidth.mobile && (
+        <>
+          <li className={cn('user-nav__item', {'user-nav__item--notifications': isNotifications})}>
+            <Link className='user-nav__link' to='/profile/notification' aria-label='view notifications'>
+              <NotificationsIcon />
+            </Link>
+          </li>
+          <li className='user-nav__item'>
+            <Link className='user-nav__link' to='/profile/edit' aria-label='edit your personal profile'>
+              <EditIcon />
+            </Link>
+          </li>
+        </>
+      )}
+
+
+
       <li className='user-nav__item user-nav__item--avatar'>
-        <Link className='user-nav__link' to='/profile' aria-label='go to your personal profile'>
+        <Link className='user-nav__link user-nav__link--avatar' to='/profile' aria-label='go to your personal profile'>
           <img
             className='user-nav__avatar'
             src={user.avatar}
@@ -31,7 +43,22 @@ function UserNav({user, notifications}) {
             aria-label={`avatar of ${user.name}`}
           >
           </img>
+
+          {width < breakpointWidth.mobile && (
+            <>
+              <span className='user-nav__avatar-name'>{user.name}</span>
+              <span className='user-nav__avatar-icon'><ArrowIcon /></span>
+            </>
+          )}
+
+
+
         </Link>
+
+
+
+
+
       </li>
     </ul>
   )
