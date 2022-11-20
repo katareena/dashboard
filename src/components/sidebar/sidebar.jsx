3444529e785fe38a4ebae0f0bad5811d53fbaba5
katareena <w10160177@gmail.com>
2022-11-20 13:41:57 +0300

    fixes switching sidebar-active-section when submitting a search form

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,6 +1,6 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { useWindowSize } from '../../hooks/use-window-size';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { breakpointWidth } from '../../constants/constants';
 import Logo from '../logo/logo';
 import { ReactComponent as ArrowIcon } from '../../assets/icons/arrow-icon.svg';
@@ -8,9 +8,9 @@ import './sidebar.scss';
 import cn from 'classnames';
 
 function Sidebar({sidebarMainData, sidebarMobileData}) {
-  const [opened, setOpened] = useState(null);
-  const [selected, setSelected] = useState('Home');
   const [width, ] = useWindowSize();
+  const [opened, setOpened] = useState(null);
+  let location = useLocation();
 
   const sidebarAppNav = sidebarMainData.slice(0, sidebarMainData.length - 3);
   const sidebarUserNav = sidebarMainData.slice(-3);
@@ -23,10 +23,6 @@ function Sidebar({sidebarMainData, sidebarMobileData}) {
     return setOpened(index);
   };  
 
-  function handleSelectSection(label) {
-    return selected !== label && setSelected(label);
-  };
-
   return (
     <nav className='sidebar'>
       <Logo />
@@ -36,14 +32,13 @@ function Sidebar({sidebarMainData, sidebarMobileData}) {
           <ul className='sidebar__list sidebar__list--grow'>
             {sidebarAppNav.map(({ icon, label, to}, index) => (
               <li
-                className={cn('sidebar__item', {'sidebar__item--opened': opened === index, 'sidebar__item--selected' : selected === label})}
+                className={cn('sidebar__item', {'sidebar__item--opened': opened === index, 'sidebar__item--selected' : location.pathname === (to.includes('/') ? to : `/${to}`)})}
                 key={label}
               >
                 <div className='sidebar__item-inner'>
                   <Link
                     className='sidebar__link'
                     to={to}
-                    onClick={() => handleSelectSection(label)}
                   >
                     <span className='sidebar__icon'>{icon}</span>
                     <span className='sidebar__label'>{label}</span>
@@ -71,13 +66,12 @@ function Sidebar({sidebarMainData, sidebarMobileData}) {
           <ul className='sidebar__list sidebar__list--decor sidebar__list--user'>
             {sidebarUserNav.map(({ icon, label, to }) => (
               <li
-              className={cn('sidebar__item', {'sidebar__item--selected' : selected === label})}
+              className={cn('sidebar__item', {'sidebar__item--selected' : location.pathname === (to.includes('/') ? to : `/${to}`)})}
                 key={label}>
                 <div className='sidebar__item-inner'>
                   <Link
                     className='sidebar__link'
                     to={to}
-                    onClick={() => handleSelectSection(label)}
                   >
                     <span className='sidebar__icon'>{icon}</span>
                     <span className='sidebar__label'>{label}</span>
@@ -93,11 +87,14 @@ function Sidebar({sidebarMainData, sidebarMobileData}) {
         <ul className='sidebar__list sidebar__list--mobile'>
           {sidebarMobileData.map(({ icon, label, to }) => (
             <li              
-              className={cn('sidebar__item', {'sidebar__item--selected' : selected === label})}
+              className={cn('sidebar__item', {'sidebar__item--selected' : location.pathname === (to.includes('/') ? to : `/${to}`)})}
               key={label}
             >
               <div className='sidebar__item-inner'>
-                <Link className='sidebar__link' to={to} onClick={() => handleSelectSection(label)}>
+                <Link
+                  className='sidebar__link'
+                  to={to}
+                >
                   <span className='sidebar__icon'>{icon}</span>
                   <span className='sidebar__label'>{label}</span>
                 </Link>
